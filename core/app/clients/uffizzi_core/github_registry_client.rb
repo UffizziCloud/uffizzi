b0a189cf3d584a12168cc4a0b06bf008e509d26e
moklidia <kitsunegakkou@gmail.com>
2022-03-30 15:19:38 +0300

    wip

---
+++ core/app/clients/uffizzi_core/github_registry_client.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class UffizziCore::GithubRegistryClient
+  attr_accessor :connection, :token, :registry_url
+
+  def initialize(registry_url:)
+    @registry_url = "https://#{registry_url}"
+    @connection = build_connection(@registry_url)
+  end
+
+  def manifests(image:, tag:)
+    url = "/v2/#{image}/manifests/#{tag}"
+    response = connection.get(url)
+
+    RequestResult.quiet.new(result: response.body, headers: response.headers)
+  end
+
+  private
+
+  def build_connection(registry_url)
+    Faraday.new(registry_url) do |conn|
+      conn.request(:token_auth, ENV['GITHUB_ACCESS_TOKEN'])
+      conn.request(:json)
+      conn.response(:json)
+      conn.adapter(Faraday.default_adapter)
+    end
+  end
+end
---
+++ core/app/clients/uffizzi_core/github_registry_client/request_result.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class UffizziCore::GithubRegistryClient
+  class RequestResult < Hashie::Mash
+    disable_warnings :key
+  end
+end
