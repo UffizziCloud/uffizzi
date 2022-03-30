249ba7a85903e0b64ef180bd86fde78c02cc30e6
7R41N33 <chepenkoanton2011@yandex.ru>
2022-03-30 16:01:22 +0400

    removed unnecessary code

--- core/app/lib/uffizzi_core/rbac/user_access_service.rb
+++ core/app/lib/uffizzi_core/rbac/user_access_service.rb
@@ -5,31 +5,7 @@ class UffizziCore::Rbac::UserAccessService
     true
   end
 
-  def developer_access_to_account?(_user, _account)
-    true
-  end
-
-  def viewer_access_to_account?(_user, _account)
-    true
-  end
-
-  def admin_or_developer_access_to_account?(_user, _account)
-    true
-  end
-
   def any_access_to_account?(_user, _account)
-    true
-  end
-
-  def admin_access_to_project?(_user, _project)
-    true
-  end
-
-  def developer_access_to_project?(_user, _project)
-    true
-  end
-
-  def viewer_access_to_project?(_user, _project)
     true
   end
 
