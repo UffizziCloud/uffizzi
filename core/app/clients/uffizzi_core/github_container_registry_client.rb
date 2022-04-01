e95c89a310e96d638ea8c22fd259fd134cce29e9
moklidia <kitsunegakkou@gmail.com>
2022-04-01 17:36:27 +0300

    implemented ghcr authentication and manifests pull

--- core/app/clients/uffizzi_core/github_container_registry_client.rb
+++ core/app/clients/uffizzi_core/github_container_registry_client.rb
@@ -1,25 +1,50 @@
 # frozen_string_literal: true
 
 class UffizziCore::GithubContainerRegistryClient
-  attr_accessor :connection, :token, :registry_url
+  attr_accessor :token, :registry_url
 
-  def initialize(registry_url:, username:, password:)
+  def initialize(registry_url:, image:, username:, password:)
     @registry_url = registry_url
-    @connection = build_connection(@registry_url, username, password)
+    @username = username
+    @password = password
+    @image = image
+    @token = access_token&.result&.token
   end
 
-  def manifests(image:, tag:)
-    url = "/v2/#{image}/manifests/#{tag}"
-    response = connection.get(url)
+  def access_token
+    service = URI.parse(registry_url).hostname
+    url = "/token?service=#{service}&scope=repository:#{@username}/#{@image}:pull"
+
+    response = connection.get(url, {})
+
+    RequestResult.new(result: response.body)
+  end
+
+  def authentificated?
+    token.present?
+  end
+
+  def manifests(tag:)
+    url = "/v2/#{@username}/#{@image}/manifests/#{tag}"
+    response = token_connection.get(url)
 
     RequestResult.quiet.new(result: response.body, headers: response.headers)
   end
 
   private
 
-  def build_connection(registry_url, username, password)
+  def connection
     Faraday.new(registry_url) do |conn|
-      conn.request(:basic_auth, username, password)
+      conn.request(:basic_auth, @username, @password)
+      conn.request(:json)
+      conn.response(:json)
+      conn.adapter(Faraday.default_adapter)
+    end
+  end
+
+  def token_connection
+    Faraday.new(registry_url) do |conn|
+      conn.request(:authorization, 'Bearer', token)
       conn.request(:json)
       conn.response(:json)
       conn.adapter(Faraday.default_adapter)
