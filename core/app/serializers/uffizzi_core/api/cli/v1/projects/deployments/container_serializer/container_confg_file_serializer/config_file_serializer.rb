3ccfe1fd3bac477479c2bd8ac191037f4f3d1a04
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2022-04-04 17:09:52 +0400

    removed unused serializer

--- core/app/serializers/uffizzi_core/api/cli/v1/projects/deployments/container_serializer/container_confg_file_serializer/config_file_serializer.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class UffizziCore::Api::Cli::V1::Projects::Deployments::ContainerSerializer::ContainerConfigFileSerializer::ConfigFileSerializer <
-  UffizziCore::BaseSerializer
-  attributes :id, :filename, :kind, :payload
-end
