10d587aad0d18f97fce10e9d8cadb34709840c37
Sergey Kirillov <sergey.kirillov@dualbootpartners.com>
2022-04-12 17:50:05 +0400

    Added created_at and updated_at fields to the secrets response

--- core/app/serializers/uffizzi_core/api/cli/v1/projects/secret_serializer.rb
+++ core/app/serializers/uffizzi_core/api/cli/v1/projects/secret_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class UffizziCore::Api::Cli::V1::Projects::SecretSerializer < UffizziCore::BaseSerializer
-  attributes :name
+  attributes :name, :created_at, :updated_at
 end
