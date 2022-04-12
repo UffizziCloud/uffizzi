ea0633d64090a3429b8931c90310fa569a186d0d
Sergey Kirillov <sergey.kirillov@dualbootpartners.com>
2022-04-12 16:56:31 +0400

    Changed serialization and bulk_create API entry

---
+++ core/app/forms/uffizzi_core/api/cli/v1/secret/bulk_assign_form.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class UffizziCore::Api::Cli::V1::Secret::BulkAssignForm
+  include ApplicationFormWithoutActiveRecord
+
+  attribute :secrets, Array
+  validate :check_duplicates
+
+  def assign_secrets(new_secrets)
+    return if new_secrets.blank?
+
+    new_secrets.each do |new_secret|
+      secret = Secret.new(name: new_secret['name'], value: new_secret['value'])
+      secrets.append(secret)
+    end
+  end
+
+  private
+
+  def check_duplicates
+    duplicates = []
+    groupped_secrets = secrets.group_by { |secret| secret['name'] }
+    groupped_secrets.each_pair do |key, value|
+      duplicates << key if value.size > 1
+    end
+
+    errors.add(:secrets, :duplicates_exist, secrets: duplicates.join(', ')) if duplicates.present?
+  end
+end
---
+++ core/app/serializers/uffizzi_core/api/cli/v1/projects/secret_serializer.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class UffizziCore::Api::Cli::V1::Projects::SecretSerializer < UffizziCore::BaseSerializer
+  attributes :name
+end
