a42d9da947fa66199e2a4c8fb433d3ea8f9bad37
Sergey Kirillov <sergey.kirillov@dualbootpartners.com>
2022-04-12 17:41:27 +0400

    Small update after rubocop

--- db/migrate/20220412133607_add_resource_to_secrets.uffizzi_core.rb
+++ db/migrate/20220412133607_add_resource_to_secrets.uffizzi_core.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This migration comes from uffizzi_core (originally 20220329124542)
 
 class AddResourceToSecrets < ActiveRecord::Migration[6.1]
--- db/migrate/20220412133608_remove_project_ref_from_secrets.uffizzi_core.rb
+++ db/migrate/20220412133608_remove_project_ref_from_secrets.uffizzi_core.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This migration comes from uffizzi_core (originally 20220329143241)
 
 class RemoveProjectRefFromSecrets < ActiveRecord::Migration[6.1]
