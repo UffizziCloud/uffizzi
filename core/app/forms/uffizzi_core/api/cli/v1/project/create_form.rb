de1d36d2183fdf2d8e919ec022fd632fa2bc5d09
moklidia <kitsunegakkou@gmail.com>
2022-04-29 12:19:28 +0300

    [85] improved error handling

--- core/app/forms/uffizzi_core/api/cli/v1/project/create_form.rb
+++ core/app/forms/uffizzi_core/api/cli/v1/project/create_form.rb
@@ -4,7 +4,4 @@ class UffizziCore::Api::Cli::V1::Project::CreateForm < UffizziCore::Project
   include UffizziCore::ApplicationForm
 
   permit :name, :slug, :description
-
-  validates :name, presence: true, uniqueness: { scope: :account }
-  validates :slug, presence: true, uniqueness: true
 end
