12dee1e1671aca4f1852aab28bf35008495a401c
Sergey Kirillov <sergey.kirillov@dualbootpartners.com>
2022-06-08 20:16:12 +0400

    Moved user_project_roles enums to the core settings

--- core/lib/uffizzi_core.rb
+++ core/lib/uffizzi_core.rb
@@ -59,4 +59,5 @@ module UffizziCore
     users_roles: :uffizzi_core_users_roles,
   }
   mattr_accessor :user_creation_sources, default: [:system, :online_registration, :google, :sso]
+  mattr_accessor :user_project_roles, default: [:admin, :developer, :viewer]
 end
