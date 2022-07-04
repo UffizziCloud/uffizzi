a4c5e78c86e2a8083bc11f7571e8f2d8c1abc77f
Anton Lubin <anton.lubin@dualbootpartners.com>
2022-07-04 16:11:59 +0500

    [UffizziCloud/uffizzi_platform#70] Add new user scopes

--- core/app/repositories/uffizzi_core/user_repo.rb
+++ core/app/repositories/uffizzi_core/user_repo.rb
@@ -7,5 +7,11 @@ module UffizziCore::UserRepo
     scope :by_email, ->(email) {
       where('lower(email) = ?', email.downcase)
     }
+    scope :with_single_account, -> {
+      joins(:memberships).group('users.id').having('count(memberships.id) = 1')
+    }
+    scope :with_account, ->(account_id) {
+      joins(:memberships).where(memberships: { account_id: account_id })
+    }
   end
 end
