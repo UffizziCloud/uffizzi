21673961135d99ad3db5213d5fc496f16f087ef2
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2022-07-04 18:11:06 +0400

    [uffizzi_platform#52] added migration for deployments table

---
+++ core/db/migrate/20220704135629_add_disabled_at_to_deployments.rb
@@ -0,0 +1,16 @@
+class AddDisabledAtToDeployments < ActiveRecord::Migration[6.1]
+  def up
+    change_table :uffizzi_core_deployments do |t|
+      t.datetime :disabled_at
+    end
+    UffizziCore::Deployment.disabled.find_each do |deployment|
+      deployment.update_attribute(:disabled_at, deployment.updated_at)
+    end
+  end
+
+  def down
+    change_table :uffizzi_core_deployments do |t|
+      t.remove :disabled_at
+    end
+  end
+end
