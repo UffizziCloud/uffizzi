7fb6d737d8f47a67ef533dcda2f3d897e5ce8510
Anton Lubin <anton.lubin@dualbootpartners.com>
2022-07-08 18:07:37 +0500

    [UffizziCloud/uffizzi_platform#97] Delete platform specific scopes

--- core/app/repositories/uffizzi_core/compose_file_repo.rb
+++ core/app/repositories/uffizzi_core/compose_file_repo.rb
@@ -4,8 +4,5 @@ module UffizziCore::ComposeFileRepo
   extend ActiveSupport::Concern
 
   included do
-    scope :with_auto_deploy, -> {
-      where(auto_deploy: UffizziCore::ComposeFile::STATE_ENABLED)
-    }
   end
 end
--- core/app/repositories/uffizzi_core/deployment_repo.rb
+++ core/app/repositories/uffizzi_core/deployment_repo.rb
@@ -7,18 +7,7 @@ module UffizziCore::DeploymentRepo
     scope :with_name, ->(name) {
       where(name: name)
     }
-    scope :by_config_file, ->(config_file) {
-      container_config_files = UffizziCore::ContainerConfigFile.where(config_file: config_file)
-      containers = UffizziCore::Container.where(id: container_config_files.select(:container_id))
-      where(id: containers.select(:deployment_id))
-    }
     scope :with_amazon_repos, -> { includes(containers: [:repo]).where(containers: { repos: { type: UffizziCore::Repo::Amazon.name } }) }
-    scope :by_templates, ->(templates) {
-      where(template: templates).where.not(templates: nil)
-    }
-    scope :with_containers, ->(source, image, tag) {
-      includes(containers: :repo).where(containers: { image: image, tag: tag, repos: { type: source } })
-    }
     scope :existed, -> { where(state: [:active, :failed]) }
   end
 end
