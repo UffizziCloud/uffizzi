9ccde90da5cfd67c7121dd50b0a4d6a8baf4b783
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2022-07-08 13:40:25 +0400

    [uffizzi_platform#52] improved migration code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_135751) do
+ActiveRecord::Schema.define(version: 2022_07_08_093405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
