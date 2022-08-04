e414c59bd19c40a0260c692f5908e93b440fe23f
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2022-08-04 18:34:56 +0400

    [#216] fixed updating preview which was not created with compose file via cli

--- core/app/services/uffizzi_core/deployment_service.rb
+++ core/app/services/uffizzi_core/deployment_service.rb
@@ -38,9 +38,13 @@ class UffizziCore::DeploymentService
 
       ActiveRecord::Base.transaction do
         deployment.containers.destroy_all
-        deployment.compose_file.destroy! if deployment.compose_file.kind.temporary?
+        deployment.compose_file.destroy! if deployment.compose_file&.kind&.temporary?
 
-        deployment.update!(containers: deployment_form.containers, compose_file_id: compose_file.id)
+        deployment.update!(
+          containers: deployment_form.containers,
+          compose_file_id: compose_file.id,
+          creation_source: UffizziCore::Deployment.creation_source.compose_file_manual
+        )
       end
 
       deployment
