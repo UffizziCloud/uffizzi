cbca6a12615f1bca29aa1f755eafc2784beb523b
Anton Lubin <anton.lubin@dualbootpartners.com>
2022-08-09 20:28:52 +0500

    [#227] Fix style offences

--- core/test/services/image_parser_service_test.rb
+++ core/test/services/image_parser_service_test.rb
@@ -6,28 +6,28 @@ class UffizziCore::ImageParserServiceTest < ActiveSupport::TestCase
   test '#parse' do
     image_parser_service = UffizziCore::ComposeFile::Parsers::Services::ImageParserService
     assert_equal(
-        {:registry_url=>nil, :namespace=>'library', :name=>'redis', :tag=>'latest'},
-        image_parser_service.parse('redis'),
+      { registry_url: nil, namespace: 'library', name: 'redis', tag: 'latest' },
+      image_parser_service.parse('redis'),
     )
     assert_equal(
-        {:registry_url=>nil, :namespace=>'library', :name=>'redis', :tag=>'5'},
-        image_parser_service.parse('redis:5'),
+      { registry_url: nil, namespace: 'library', name: 'redis', tag: '5' },
+      image_parser_service.parse('redis:5'),
     )
     assert_equal(
-        {:registry_url=>nil, :namespace=>'namespace', :name=>'redis', :tag=>'latest'},
-        image_parser_service.parse('namespace/redis'),
+      { registry_url: nil, namespace: 'namespace', name: 'redis', tag: 'latest' },
+      image_parser_service.parse('namespace/redis'),
     )
     assert_equal(
-        {:registry_url=>'docker.io:443', :namespace=>'namespace', :name=>'redis', :tag=>'latest'},
-        image_parser_service.parse('docker.io/namespace/redis'),
+      { registry_url: 'docker.io:443', namespace: 'namespace', name: 'redis', tag: 'latest' },
+      image_parser_service.parse('docker.io/namespace/redis'),
     )
     assert_equal(
-        {:registry_url=>'my_private.registry:5000', :namespace=>'namespace', :name=>'redis', :tag=>'5.3'},
-        image_parser_service.parse('my_private.registry:5000/namespace/redis:5.3'),
+      { registry_url: 'my_private.registry:5000', namespace: 'namespace', name: 'redis', tag: '5.3' },
+      image_parser_service.parse('my_private.registry:5000/namespace/redis:5.3'),
     )
     assert_equal(
-        {:registry_url=>'localhost:80', :namespace=>nil, :name=>'redis', :tag=>'5.3'},
-        image_parser_service.parse('localhost:80/redis:5.3'),
+      { registry_url: 'localhost:80', namespace: nil, name: 'redis', tag: '5.3' },
+      image_parser_service.parse('localhost:80/redis:5.3'),
     )
   end
 
