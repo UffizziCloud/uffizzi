72ac98f879ee30759d29e13dee7d9b3f68415370
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2022-08-17 18:08:14 +0400

    [#221] migration recreated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_05_164944) do
+ActiveRecord::Schema.define(version: 2022_08_17_140346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
