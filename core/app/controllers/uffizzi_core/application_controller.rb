cd568e8a4ca4d678d878b5ee6f55038acdfcfd5a
moklidia <kitsunegakkou@gmail.com>
2022-08-24 21:15:17 +0200

    fixed pundit inclusion

--- core/app/controllers/uffizzi_core/application_controller.rb
+++ core/app/controllers/uffizzi_core/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UffizziCore::ApplicationController < ActionController::Base
-  include Pundit::Authorization
+  include Pundit
   include UffizziCore::ResponseService
   include UffizziCore::AuthManagement
   include UffizziCore::AuthorizationConcern
