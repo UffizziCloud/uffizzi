62e65687c143b27abe7b96a0bfd345f7a629725e
moklidia <kitsunegakkou@gmail.com>
2022-08-26 19:34:13 +0200

    [76] added account info to the projects response, refactoring

--- core/app/contexts/uffizzi_core/base_context.rb
+++ core/app/contexts/uffizzi_core/base_context.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class UffizziCore::BaseContext
-  attr_reader :user, :user_access_module, :params, :account
+  attr_reader :user, :user_access_module, :params
 
   def initialize(user, user_access_module, params)
     @user = user
     @user_access_module = user_access_module
-    @account = user.organizational_account
     @params = params
   end
 end
---
+++ core/app/controllers/uffizzi_core/api/cli/v1/accounts/projects_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# @resource Project
+
+class UffizziCore::Api::Cli::V1::Accounts::ProjectsController < UffizziCore::Api::Cli::V1::Accounts::ApplicationController
+  before_action :authorize_uffizzi_core_api_cli_v1_accounts_projects
+
+  # Create a project
+  #
+  # @path [POST] /api/cli/v1/accounts/{account_id}/projects
+  # @parameter params(required,body) [object<name: string, slug: string, description: string>]
+  #
+  # @response <object< project: Project>> 200 OK
+  # @response 404 Not Found
+  # @response 401 Not authorized
+  # @response [object<errors: object<password: string >>] 422 Unprocessable entity
+
+  def create
+    project_form = UffizziCore::Api::Cli::V1::Project::CreateForm.new(project_params)
+    project_form.account = current_user.organizational_account
+
+    if project_form.save
+      UffizziCore::ProjectService.add_users_to_project!(project_form, current_user)
+    end
+
+    respond_with project_form
+  end
+
+  private
+
+  def project_params
+    params.require(:project)
+  end
+end
