ba8291e39de8c40f6d4b3bb0928f684545ba6c02
Evgeny <evgeny.agafonov@dualbootpartners.com>
2022-09-15 20:00:04 +0300

    [#234] add test

---
+++ core/test/factories/host_volume_files.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :host_volume_file, class: UffizziCore::HostVolumeFile do
+    source { generate(:path) }
+    source { generate(:relative_path) }
+    payload { generate(:string) }
+    added_by { nil }
+    project { nil }
+    compose_file { nil }
+  end
+end
