abdd4542f3691585d757ec65ea45e377a4ad018f
jessika <castellanojessika@gmail.com>
2022-09-21 15:09:11 -0400

    [#264] Add credential type to unable to login message

--- core/app/models/uffizzi_core/credential.rb
+++ core/app/models/uffizzi_core/credential.rb
@@ -2,4 +2,15 @@
 
 class UffizziCore::Credential < UffizziCore::ApplicationRecord
   include UffizziCore::Concerns::Models::Credential
+  extend Enumerize
+
+  enumerize :type,
+            in: [
+              UffizziCore::Credential::GithubContainerRegistry.name,
+              UffizziCore::Credential::DockerHub.name,
+              UffizziCore::Credential::DockerRegistry.name,
+              UffizziCore::Credential::Azure.name,
+              UffizziCore::Credential::Google.name,
+              UffizziCore::Credential::Amazon.name,
+            ], i18n_scope: ['enumerize.credential.type']
 end
