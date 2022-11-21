a7279b841bf58afd8df7c55b6cf1793bf014bb97
Jessika castellano <justice@Jessikas-MacBook-Pro.local>
2022-11-21 08:21:20 -0300

    [#351] add valid token to delegation

--- core/app/services/uffizzi_core/user_access_service.rb
+++ core/app/services/uffizzi_core/user_access_service.rb
@@ -6,7 +6,7 @@ class UffizziCore::UserAccessService
   delegate :admin_access_to_account?, :developer_access_to_account?, :viewer_access_to_account?,
            :admin_or_developer_access_to_account?, :any_access_to_account?, :admin_access_to_project?,
            :developer_access_to_project?, :viewer_access_to_project?, :admin_or_developer_access_to_project?,
-           :any_access_to_project?, :global_admin?, to: :@user_access_module
+           :any_access_to_project?, :global_admin?, :valid_token?, to: :@user_access_module
 
   def initialize(user_access_module)
     @user_access_module = user_access_module
