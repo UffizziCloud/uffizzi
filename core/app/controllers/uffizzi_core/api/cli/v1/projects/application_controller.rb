4484eafae492906341fe74ab631d6c0ab525dd88
Anton Lubin <anton.lubin@dualbootpartners.com>
2022-11-25 22:42:09 +0500

    [#316] Handle registry clients errors

--- core/app/controllers/uffizzi_core/api/cli/v1/projects/application_controller.rb
+++ core/app/controllers/uffizzi_core/api/cli/v1/projects/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UffizziCore::Api::Cli::V1::Projects::ApplicationController < UffizziCore::Api::Cli::V1::ApplicationController
+  rescue_from Faraday::ClientError, with: :handle_container_registry_client_error
+
   def resource_project
     @resource_project ||= current_user.projects.find_by!(slug: params[:project_slug])
   end
@@ -11,5 +13,9 @@ class UffizziCore::Api::Cli::V1::Projects::ApplicationController < UffizziCore::
 
   def policy_context
     UffizziCore::ProjectContext.new(current_user, user_access_module, resource_project, resource_account, params)
+  end
+
+  def handle_container_registry_client_error(exception)
+    render json: { errors: exception.response[:body][:errors] }, status: exception.response[:status]
   end
 end
