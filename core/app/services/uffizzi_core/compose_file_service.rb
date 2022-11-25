395794d3de833ef5ce847de4af38084c3a91b25f
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2022-11-25 17:30:30 +0400

    [#317] improved handling of Psych::SyntaxError

--- core/app/services/uffizzi_core/compose_file_service.rb
+++ core/app/services/uffizzi_core/compose_file_service.rb
@@ -186,8 +186,9 @@ class UffizziCore::ComposeFileService
     def load_compose_data(compose_content)
       begin
         compose_data = YAML.safe_load(compose_content, aliases: true)
-      rescue Psych::SyntaxError
-        raise UffizziCore::ComposeFile::ParseError, I18n.t('compose.invalid_file')
+      rescue Psych::SyntaxError => e
+        error_message = remove_filename_from_error_message(e.message)
+        raise UffizziCore::ComposeFile::ParseError, error_message
       end
       raise UffizziCore::ComposeFile::ParseError, I18n.t('compose.unsupported_file') if compose_data.nil?
 
@@ -202,6 +203,11 @@ class UffizziCore::ComposeFileService
 
         raise UffizziCore::ComposeFile::ParseError, I18n.t('compose.invalid_config_option', value: option)
       end
+    end
+
+    def remove_filename_from_error_message(error_message)
+      error_message.slice!('(<unknown>): ')
+      error_message
     end
   end
 end
