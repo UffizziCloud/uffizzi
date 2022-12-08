795b3e012f0cd95677df939f753604173ab702f9
Anton Lubin <anton.lubin@dualbootpartners.com>
2022-12-08 19:08:22 +0500

    [#316] Extend Error class and shorten Handling concern

--- core/app/errors/uffizzi_core/container_registry_error.rb
+++ core/app/errors/uffizzi_core/container_registry_error.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class UffizziCore::ContainerRegistryError < Faraday::ClientError
+  def initialize(exc, response = nil)
+    exc_msg_and_response!(exc, response)
+    super(prepare_message)
+  end
+
+  private
+
+  def prepare_message
+    response_body_ = response_body.empty? ? {} : JSON.parse(response_body, symbolize_names: true)
+    errors = if response_body_.has_key?(:errors)
+      convert_errors_array_to_object(response_body_[:errors])
+    else
+      { registry_error: [I18n.t('registry.error', code: response_status)] }
+    end
+
+    errors.to_json
+  end
+
+  def convert_errors_array_to_object(array)
+    array.reduce({}) { |acc, error| acc.merge(error[:code] => [error[:message], error[:detail].to_json]) }
+  end
 end
