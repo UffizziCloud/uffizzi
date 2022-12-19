dce3843f0f33bf9e72aae51c15940df8eaaa6d72
Lidia Mokevnina <kitsunegakkou@gmail.com>
2022-12-19 15:21:47 +0300

    [340] renamed param name

--- core/app/controllers/uffizzi_core/api/cli/v1/ci/sessions_controller.rb
+++ core/app/controllers/uffizzi_core/api/cli/v1/ci/sessions_controller.rb
@@ -31,6 +31,6 @@ class UffizziCore::Api::Cli::V1::Ci::SessionsController < UffizziCore::Api::Cli:
   private
 
   def user_params
-    params.require(:user).permit(:token, :pipeline_token)
+    params.require(:user).permit(:token, :access_token)
   end
 end
