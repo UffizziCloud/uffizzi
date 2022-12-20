4c6a4f9a1853d6d44f3d838d93a0510e5389cb75
Anton Lubin <anton.lubin@dualbootpartners.com>
2022-12-20 21:16:35 +0500

    [#316] Check for response code on determining error type

--- core/app/errors/uffizzi_core/container_registry_error.rb
+++ core/app/errors/uffizzi_core/container_registry_error.rb
@@ -17,7 +17,7 @@ class UffizziCore::ContainerRegistryError < Faraday::ClientError
 
   def prepare_message
     response_body_ = response_body.empty? ? {} : JSON.parse(response_body, symbolize_names: true)
-    errors = if response_body_.has_key?(:errors)
+    errors = if response_body_.has_key?(:errors) && [401, 403].exclude?(response_status)
       convert_errors_array_to_object(response_body_[:errors])
     else
       { registry_error: I18n.t('registry.error', code: response_status) }
