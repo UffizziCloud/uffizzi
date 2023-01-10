3ab43253143867bec74615fdf16a7dd2b2bd4ff1
Artem Dunaev <artem.dunaev@dualbootpartners.com>
2023-01-10 16:42:34 +0200

    [#351] fix serializer

--- core/app/serializers/uffizzi_core/api/cli/v1/user_serializer.rb
+++ core/app/serializers/uffizzi_core/api/cli/v1/user_serializer.rb
@@ -6,6 +6,6 @@ class UffizziCore::Api::Cli::V1::UserSerializer < UffizziCore::BaseSerializer
   attributes :default_account
 
   def default_account
-    UffizziCore::Api::Cli::V1::UserSerializer::AccountSerializer.new(current_user.default_account)
+    UffizziCore::Api::Cli::V1::UserSerializer::AccountSerializer.new(object.default_account)
   end
 end
