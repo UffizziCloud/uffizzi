c9403c95daf8fcbb16281527ff9ce2ab6a688dbf
Anton Lubin <anton.lubin@dualbootpartners.com>
2023-02-08 21:53:41 +0500

    [#377] Update schemas

--- db/schema.rb
+++ db/schema.rb
@@ -336,8 +336,6 @@ ActiveRecord::Schema.define(version: 2023_02_07_061703) do
     t.boolean "share_to_github"
     t.integer "delete_preview_after"
     t.string "tag_pattern_deprecated"
-    t.boolean "deploy_preview_when_pull_request_is_opened"
-    t.boolean "delete_preview_when_pull_request_is_closed"
     t.index ["project_id"], name: "index_repos_on_project_id"
   end
 
