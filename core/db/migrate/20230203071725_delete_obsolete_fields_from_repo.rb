ac49d2c7841ccf1ba3c50bdad461cac153f93fc2
Anton Lubin <anton.lubin@dualbootpartners.com>
2023-02-08 22:10:20 +0500

    [#377] Lint

--- core/db/migrate/20230203071725_delete_obsolete_fields_from_repo.rb
+++ core/db/migrate/20230203071725_delete_obsolete_fields_from_repo.rb
@@ -2,6 +2,7 @@
 
 class DeleteObsoleteFieldsFromRepo < ActiveRecord::Migration[6.1]
   def change
-    remove_columns :uffizzi_core_repos, :deploy_preview_when_pull_request_is_opened, :delete_preview_when_pull_request_is_closed, type: :boolean
+    remove_columns :uffizzi_core_repos, :deploy_preview_when_pull_request_is_opened, :delete_preview_when_pull_request_is_closed,
+                   type: :boolean
   end
 end
--- db/migrate/20230203134111_delete_obsolete_fields_from_repo.uffizzi_core.rb
+++ db/migrate/20230203134111_delete_obsolete_fields_from_repo.uffizzi_core.rb
@@ -3,6 +3,7 @@
 # This migration comes from uffizzi_core (originally 20230203071725)
 class DeleteObsoleteFieldsFromRepo < ActiveRecord::Migration[6.1]
   def change
-    remove_columns :uffizzi_core_repos, :deploy_preview_when_pull_request_is_opened, :delete_preview_when_pull_request_is_closed, type: :boolean
+    remove_columns :uffizzi_core_repos, :deploy_preview_when_pull_request_is_opened, :delete_preview_when_pull_request_is_closed,
+                   type: :boolean
   end
 end
