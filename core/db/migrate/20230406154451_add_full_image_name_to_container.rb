f12699029a32f878d395f3e383c0665b8e6b8096
Lidia Mokevnina <kitsunegakkou@gmail.com>
2023-04-10 11:42:07 +0200

    [968] updated migrations

--- core/db/migrate/20230406154451_add_full_image_name_to_container.rb
+++ core/db/migrate/20230406154451_add_full_image_name_to_container.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class AddFullImageNameToContainer < ActiveRecord::Migration[6.1]
-  def up
+  def change
     add_column(:uffizzi_core_containers, :full_image_name, :string)
-  end
-
-  def down
-    remove_column(:uffizzi_core_containers, :full_image_name)
   end
 end
--- db/migrate/20230406154547_add_full_image_name_to_container.uffizzi_core.rb
+++ db/migrate/20230406154547_add_full_image_name_to_container.uffizzi_core.rb
@@ -2,11 +2,7 @@
 
 # This migration comes from uffizzi_core (originally 20230406154451)
 class AddFullImageNameToContainer < ActiveRecord::Migration[6.1]
-  def up
+  def change
     add_column(:uffizzi_core_containers, :full_image_name, :string)
-  end
-
-  def down
-    remove_column(:uffizzi_core_containers, :full_image_name)
   end
 end
