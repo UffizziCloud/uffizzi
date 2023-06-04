e4dbb26e69378a991e374effd01f4321cdbc73e8
Lidia Mokevnina <kitsunegakkou@gmail.com>
2023-06-04 12:00:18 +0200

    [425] fixed the Faraday trailing slash trimming

--- core/app/clients/uffizzi_core/docker_registry_client.rb
+++ core/app/clients/uffizzi_core/docker_registry_client.rb
@@ -16,14 +16,14 @@ class UffizziCore::DockerRegistryClient
   end
 
   def authenticated?
-    response = @connection.head('/v2/')
+    @connection.head("#{@registry_url}/v2/")
 
-    [401, 400].exclude?(response.status)
+    true
   end
 
   def manifests(image:, tag:, namespace: nil)
     full_image = [namespace, image].compact.join('/')
-    url = "/v2/#{full_image}/manifests/#{tag}"
+    url = "#{@registry_url}/v2/#{full_image}/manifests/#{tag}"
     response = @connection.get(url)
 
     RequestResult.new(status: response.status, result: response.body)
@@ -31,13 +31,15 @@ class UffizziCore::DockerRegistryClient
 
   private
 
-  def build_connection(registry_url, username, password)
-    connection = Faraday.new(registry_url) do |faraday|
+  def build_connection(username, password)
+    # initializing Faraday with the registry_url will trim the trailing slash required for the /v2/ request
+    connection = Faraday.new do |faraday|
       faraday.headers['Accept'] = ACCEPTED_TYPES
       faraday.request(:basic_auth, username, password) if username.present? && password.present?
       faraday.request(:json)
       faraday.response(:json)
       faraday.response(:follow_redirects)
+      faraday.response(:raise_error)
       faraday.adapter(Faraday.default_adapter)
     end
 
