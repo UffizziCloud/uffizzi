5016b2f81c5afc662cfb76b22e6eff1210209a47
Zipofar <zipofar@gmail.com>
2023-07-27 13:07:43 +0300

    [460] Fix bug: add dns_managed_zone to controller settings

--- core/app/serializers/uffizzi_core/controller/create_cluster/cluster_serializer.rb
+++ core/app/serializers/uffizzi_core/controller/create_cluster/cluster_serializer.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class UffizziCore::Controller::CreateCluster::ClusterSerializer < UffizziCore::BaseSerializer
+  include UffizziCore::DependencyInjectionConcern
+
   attributes :name, :manifest, :base_ingress_host
 
   def base_ingress_host
-    [object.namespace, Settings.app.vcluster_managed_dns_zone].join('.')
+    managed_dns_zone = controller_settings_service.vcluster(object).managed_dns_zone
+
+    [object.namespace, managed_dns_zone].join('.')
   end
 end
