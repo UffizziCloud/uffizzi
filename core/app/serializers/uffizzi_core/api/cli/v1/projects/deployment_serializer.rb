283063f74b4a30c2fea3b4014a3b8af1e39c2533
Zipofar <zipofar@gmail.com>
2023-08-25 19:31:37 +0300

    [268] remove unusable data for deployment

--- core/app/serializers/uffizzi_core/api/cli/v1/projects/deployment_serializer.rb
+++ core/app/serializers/uffizzi_core/api/cli/v1/projects/deployment_serializer.rb
@@ -7,19 +7,9 @@ class UffizziCore::Api::Cli::V1::Projects::DeploymentSerializer < UffizziCore::B
   type :deployment
 
   attributes :id,
-             :kind,
              :project_id,
-             :created_at,
-             :updated_at,
              :state,
-             :preview_url,
-             :tag,
-             :branch,
-             :image_id,
-             :ingress_container_ready,
-             :ingress_container_state,
-             :creation_source,
-             :metadata
+             :preview_url
 
   has_many :containers
 
@@ -27,39 +17,5 @@ class UffizziCore::Api::Cli::V1::Projects::DeploymentSerializer < UffizziCore::B
 
   def containers
     object.containers.active
-  end
-
-  def tag
-    object.ingress_container&.tag
-  end
-
-  def branch
-    object.ingress_container&.repo&.branch
-  end
-
-  def image_id
-    object.ingress_container&.repo&.name
-  end
-
-  def ingress_container_ready
-    !!object.ingress_container&.activity_items&.last&.events&.last&.deployed?
-  end
-
-  def ingress_container_state
-    last_event = object.ingress_container&.activity_items&.last&.events&.last
-
-    case last_event&.state
-    when UffizziCore::Event.state.deployed
-      :deployed
-    when UffizziCore::Event.state.failed, UffizziCore::Event.state.timeout, UffizziCore::Event.state.cancelled
-      :failed
-    else
-      state_from_activity_items
-    end
-  end
-
-  def state_from_activity_items
-    activity_items_count = object.ingress_container&.activity_items&.count
-    activity_items_count.to_i > 1 ? :updating : :pending
   end
 end
